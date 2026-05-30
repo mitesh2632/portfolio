ed5d361e468bae37524e40c254a963205105fbb9
Mitesh <meetmakwana76@gmail.com>
2026-05-30 16:04:25 +0530

    fix: All filter issue

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,6 @@ export const metadata = {
   description:
     "This is the portfolio of Mitesh Makwana. a Front End Developer with 3 years of hands-on experience crafting and enhancing web applications. Proficient in HTML, CSS, and JavaScript, I specialize in leveraging cutting-edge frameworks like Vue JS, Angular JS, Next.js, Nest JS and libraries like React and also create mobile Applications in React-native to create seamless user experiences. With a keen eye for detail and a passion for clean, efficient code, I strive to deliver innovative solutions that exceed expectations",
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
